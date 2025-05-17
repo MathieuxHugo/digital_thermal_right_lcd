53292f130bb5489dbc6dcd91bd20418a7b92edae
HugoMathieux <mathieuxhugo@gmail.com>
2025-05-18 00:08:35 +0200

    Refactor LED index management: Move leds_indexes to a new module for better organization and maintainability

---
+++ src/indexes.py
@@ -0,0 +1,16 @@
+leds_indexes = {
+    "cpu" : list(range(0, 42)),
+    "gpu" : list(range(42, 84)),
+    "cpu_led": list(range(0, 2)),
+    "cpu_temp": list(range(2, 23)),
+    "cpu_celsius": 23,
+    "cpu_fahrenheit": 24,
+    "cpu_usage": list(range(25, 41)),
+    "cpu_percent_led": 41,
+    "gpu_led": [82, 83],
+    "gpu_temp": list(range(81, 60, -1)),
+    "gpu_celsius": 59,
+    "gpu_fahrenheit": 60,
+    "gpu_usage": list(range(58, 42, -1)),
+    "gpu_percent_led": 42,
+}
